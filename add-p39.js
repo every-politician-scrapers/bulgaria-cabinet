d47e97ab06d8f1603d3982d5388a8ad42463546c
Tony Bowden <tony@tmtm.com>
2021-08-04 10:09:37 +0300

    Update to every-politician-scraper 0.0.23

--- add-p39.js
+++ add-p39.js
@@ -1,26 +1,21 @@
-module.exports = (id, position, personname, positionname) => {
-  reference = {
-    P854: 'https://www.gov.bg/en/Cabinet/CABINET-MEMBERS',
-    P1476: {
-      text: 'CARETAKER CABINET MEMBERS',
-      language: 'en',
-    },
-    P813: new Date().toISOString().split('T')[0],
-    P407: 'Q1860', // language: English
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (id, position, startdate) => {
+  qualifier = {
+    P580: meta.cabinet.start,
+    P5054: meta.cabinet.id,
   }
-  if(personname)     reference['P1810'] = personname
-  if(positionname)   reference['P1932'] = positionname
+
+  if(startdate)      qualifier['P580']  = startdate
 
   return {
     id,
     claims: {
       P39: {
         value: position,
-        qualifiers: {
-          P580: '2021-05-12',
-          P5054: 'Q106803620' // Yanev Government
-        },
-        references: reference,
+        qualifiers: qualifier,
       }
     }
   }
--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/scraper_data'
-require 'open-uri/cached'
 require 'pry'
 
 class MemberList
@@ -41,5 +40,5 @@ class MemberList
   end
 end
 
-url = 'https://www.gov.bg/en/Cabinet/CABINET-MEMBERS'
-puts EveryPoliticianScraper::ScraperData.new(url).csv
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
