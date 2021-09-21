9023ce62a3f7d938ec10f88cfb4a723474fae77b
Tony Bowden <tony@tmtm.com>
2021-09-21 09:05:53 +0300

    Don't report PM as missing from diff
    This position isn't included in the list of ministers.

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,12 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+  ['---', 'Stefan Janev', 'Prime Minister of Bulgaria']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
+
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
