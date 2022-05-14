58c0050718b8e8b93c17bd22c1c1c9c49bdf5766
Tony Bowden <tony@tmtm.com>
2022-05-14 11:04:17 +0300

    move official comparison to sources/

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -8,7 +8,7 @@ SKIP = [
   ['---', 'Stefan Janev', 'Prime Minister of Bulgaria']
 ].freeze
 
-diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
-                                         .reject { |row| SKIP.include? row }
+diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+                                                 .reject { |row| SKIP.include? row }
 
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
