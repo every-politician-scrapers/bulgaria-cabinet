eefcdb465934d86603fde34f71b3da465e0aef74
Tony Bowden <tony@tmtm.com>
2022-06-10 07:15:47 +0300

    update to latest wpoffice

--- sources/youth-sport-bgwiki/scraper.rb
+++ sources/youth-sport-bgwiki/scraper.rb
@@ -4,29 +4,6 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-# Bulgarian dates
-class Bulgarian < WikipediaDate
-  REMAP = {
-    '…'         => '',
-    'януари'    => 'January',
-    'FEBRUARY'  => 'February',
-    'март'      => 'March',
-    'APRIL'     => 'April',
-    'май'       => 'May',
-    'JUNE'      => 'June',
-    'юли'       => 'July',
-    'август'    => 'August',
-    'септември' => 'September',
-    'октомври'  => 'October',
-    'ноември'   => 'November',
-    'декември'  => 'December',
-  }.freeze
-
-  def remap
-    super.merge(REMAP)
-  end
-end
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
   decorator UnspanAllTables
@@ -39,14 +16,6 @@ class OfficeholderList < OfficeholderListBase
   class Officeholder < OfficeholderBase
     def columns
       %w[_ name dates].freeze
-    end
-
-    def tds
-      noko.css('td,th')
-    end
-
-    def date_class
-      Bulgarian
     end
   end
 end
