f971931103ca0b15da23ce61de87b9e6467b2c66
Tony Bowden <tony@tmtm.com>
2022-06-17 13:09:41 +0300

    update to DecoratedComparison

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -8,7 +8,7 @@ SKIP = [
   ['---', 'Kiril Petkov', 'Prime Minister of Bulgaria']
 ].freeze
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('wikidata.csv', 'scraped.csv').diff
                                                  .reject { |row| SKIP.include? row }
 
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
